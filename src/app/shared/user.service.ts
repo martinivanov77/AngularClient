804d877eb96176fc57e5a933a265c3efbf27846c
Martin <Martin@DESKTOP-FKIDL0O>
2020-03-10 15:37:41 +0200

    -Commiting changes

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, Validators, FormGroup } from '@angular/forms';
 import { groupBy } from 'rxjs/internal/operators/groupBy';
+import{HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,43 @@ import { groupBy } from 'rxjs/internal/operators/groupBy';
 export class UserService {
 
 public formModel:any;
+private readonly BaseURI = 'http://localhost:52291/api/account';
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb:FormBuilder, private http:HttpClient) {
+
 
      this.formModel = this.fb.group({
       UserName: ['', Validators.required],
       Passwords: this.fb.group({
-        Password: ['',Validators.required],
-        ConfirmPassword: ['', Validators.required]
-      })
+        Password: ['',[Validators.required,Validators.minLength(4)]],
+        ConfirmPassword: ['', Validators.required],
+      },{validator: this.comparePasswords})
     });
+  }
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPasswordControl = fb.get('ConfirmPassword');
+    //passwordMismatch
+    //confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPasswordControl.errors == null || 'passwordMismatch' in confirmPasswordControl.errors) {
+      if (fb.get('Password').value != confirmPasswordControl.value)
+      confirmPasswordControl.setErrors({ passwordMismatch: true });
+      else
+      confirmPasswordControl.setErrors(null);
+    }
+  }
+
+  register(){
+
+    var body = {
+      UserName: this.formModel.value.UserName,
+      Password: this.formModel.value.Passwords.Password,
+      ConfirmPassword: this.formModel.value.Passwords.ConfirmPassword
+      
+    };
+    
+    return this.http.post(this.BaseURI + '/register', body)
+
   }
 }
 
--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -10,7 +11,27 @@ export class RegistrationComponent implements OnInit {
 
   constructor(public service:UserService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void  {
+    this.service.formModel.reset();
   }
 
-}
+  onSubmit() {
+    this.service.register().subscribe(
+      (response:any) => {
+        if(response.username != null){
+          this.service.formModel.reset();
+        }else{
+          response.errors.array.forEach(element => {
+            switch(element.code){
+              case 'DupluicateUserName':
+              break;
+
+              default:
+
+              break;
+            }
+          });
+        }
+      }
+    );
+}}
